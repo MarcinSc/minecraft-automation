7d6b9bca9213c2b82ba21c24d4012b6de9de8cf6
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-02 16:58:31 +0000

    AND and OR operators.

---
+++ test/com/gempukku/minecraft/automation/lang/OperatorTest.java
@@ -0,0 +1,25 @@
+package com.gempukku.minecraft.automation.lang;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class OperatorTest extends ProgramTest {
+    @Test
+    public void orOperator() throws IllegalSyntaxException, IOException, ExecutionException {
+        assertEquals("Test", executeScript("if (true || false) return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (false || false) return \"Failed\"; else return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (true || true) return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (false || true) return \"Test\";").getValue());
+    }
+
+    @Test
+    public void andOperator() throws IllegalSyntaxException, IOException, ExecutionException {
+        assertEquals("Test", executeScript("if (true && false) return \"Failed\"; else return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (false && false) return \"Failed\"; else return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (true && true) return \"Test\";").getValue());
+        assertEquals("Test", executeScript("if (false && true) return \"Failed\"; else return \"Test\";").getValue());
+    }
+}
