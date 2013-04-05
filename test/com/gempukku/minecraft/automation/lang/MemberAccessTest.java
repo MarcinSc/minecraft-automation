7b51c818fcd708623eb6518cf60d39759ca43c59
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-05 11:11:44 +0000

    World is passed to object functions.

--- test/com/gempukku/minecraft/automation/lang/MemberAccessTest.java
+++ test/com/gempukku/minecraft/automation/lang/MemberAccessTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class MemberAccessTest extends ProgramTest {
     @Test
     public void testMemberAccess() throws ExecutionException, IllegalSyntaxException, IOException {
-        ScriptExecutable exec = new ScriptParser().parseScript(new StringReader("return os.exit();"));
+        ScriptExecutable exec = new ScriptParser().parseScript(new StringReader("return os.getModuleSlotCount();"));
         CallContext context = new CallContext(null, false, true);
 
         ObjectDefinition os = constructOS();
@@ -26,7 +26,7 @@ public class MemberAccessTest extends ProgramTest {
         while (!executionContext.isFinished())
             executionContext.executeNext();
 
-        assertEquals(0, ((Number) executionContext.getReturnValue().getValue()).intValue());
+        assertEquals(10, ((Number) executionContext.getReturnValue().getValue()).intValue());
     }
 
     private ObjectDefinition constructOS() {
