bc991e3599cae1556be82fd207d74f55fb648586
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-21 18:42:13 +0000

    Assigning correct column numbers to terms, as they get parsed.

--- test/com/gempukku/minecraft/automation/lang/OperatorTest.java
+++ test/com/gempukku/minecraft/automation/lang/OperatorTest.java
@@ -7,26 +7,26 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class OperatorTest extends ProgramTest {
-    @Test
-    public void orOperator() throws IllegalSyntaxException, IOException, ExecutionException {
-        assertEquals("Test", executeScript("if (true || false) return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (false || false) return \"Failed\"; else return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (true || true) return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (false || true) return \"Test\";").getValue());
-    }
+	@Test
+	public void orOperator() throws IllegalSyntaxException, IOException, ExecutionException {
+		assertEquals("Test", executeScript("if (true || false) return \"Test\";").getValue());
+		assertEquals("Test", executeScript("if (false || false) return \"Failed\"; else return \"Test\";").getValue());
+		assertEquals("Test", executeScript("if (true || true) return \"Test\";").getValue());
+		assertEquals("Test", executeScript("if (false || true) return \"Test\";").getValue());
+	}
 
-    @Test
-    public void andOperator() throws IllegalSyntaxException, IOException, ExecutionException {
-        assertEquals("Test", executeScript("if (true && false) return \"Failed\"; else return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (false && false) return \"Failed\"; else return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (true && true) return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (false && true) return \"Failed\"; else return \"Test\";").getValue());
-    }
-    
-    @Test
-    public void notOperator() throws IllegalSyntaxException, IOException, ExecutionException {
-        assertEquals("Test", executeScript("if (!false) return \"Test\";").getValue());
-        assertEquals("Test", executeScript("if (!true) return \"Failed\"; else return \"Test\";").getValue());
-        assertEquals("Test", executeScript("function func() { return false; } if (!func()) return \"Test\";").getValue());
-    }
+	@Test
+	public void andOperator() throws IllegalSyntaxException, IOException, ExecutionException {
+		assertEquals("Test", executeScript("  if (true && false) return \"Failed\"; else return \"Test\";").getValue());
+		assertEquals("Test", executeScript("if (false && false) return \"Failed\"; else return \"Test\";").getValue());
+		assertEquals("Test", executeScript(" if (true && true) return \"Test\";").getValue());
+		assertEquals("Test", executeScript(" if (false && true) return \"Failed\"  ;    else return \"Test\";").getValue());
+	}
+
+	@Test
+	public void notOperator() throws IllegalSyntaxException, IOException, ExecutionException {
+		assertEquals("Test", executeScript("if (!false) return \"Test\";").getValue());
+		assertEquals("Test", executeScript("if (!true) return \"Failed\"; else return \"Test\";").getValue());
+		assertEquals("Test", executeScript("function func() { return false; } if (!func()) return \"Test\";").getValue());
+	}
 }
