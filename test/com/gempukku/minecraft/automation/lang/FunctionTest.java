6f8fc248211f51854ac74953414c9a9378751cad
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-05-07 12:13:31 +0000

    Functions can call themselves recursively.

--- test/com/gempukku/minecraft/automation/lang/FunctionTest.java
+++ test/com/gempukku/minecraft/automation/lang/FunctionTest.java
@@ -8,44 +8,49 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class FunctionTest extends ProgramTest {
-	@Test
-	public void functionCalled() throws IllegalSyntaxException, IOException, ExecutionException {
-		final Variable variable = executeScript("function func() { return \"Test\"; } return func();");
-		assertEquals("Test", variable.getValue());
-	}
+    @Test
+    public void functionCalled() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("function func() { return \"Test\"; } return func();");
+        assertEquals("Test", variable.getValue());
+    }
 
-	@Test
-	public void functionNotCalled() throws IllegalSyntaxException, IOException, ExecutionException {
-		final Variable variable = executeScript("function func() { return \"Failed\"; } var v = \"Test\"; return v;");
-		assertEquals("Test", variable.getValue());
-	}
+    @Test
+    public void functionNotCalled() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("function func() { return \"Failed\"; } var v = \"Test\"; return v;");
+        assertEquals("Test", variable.getValue());
+    }
 
-	@Test
-	public void parameterGetsPassed() throws IllegalSyntaxException, IOException, ExecutionException {
-		final Variable variable = executeScript("function func(value) { return value; } return func(\"Test\");");
-		assertEquals("Test", variable.getValue());
-	}
+    @Test
+    public void parameterGetsPassed() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("function func(value) { return value; } return func(\"Test\");");
+        assertEquals("Test", variable.getValue());
+    }
 
-	@Test
-	public void multipleParameters() throws IllegalSyntaxException, IOException, ExecutionException {
-		final Variable variable = executeScript("function func(v, v2) { if (v2) return v; return \"Failed\"; } return func(\"Test\", true);");
-		assertEquals("Test", variable.getValue());
-	}
+    @Test
+    public void multipleParameters() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("function func(v, v2) { if (v2) return v; return \"Failed\"; } return func(\"Test\", true);");
+        assertEquals("Test", variable.getValue());
+    }
 
-	@Test
-	public void functionHasItsOwnScope() throws IllegalSyntaxException, IOException, ExecutionException {
-		final Variable variable = executeScript("var v = \"Failed\"; function func() { var v; if (v==null) return \"Test\"; } return func();");
-		assertEquals("Test", variable.getValue());
-	}
+    @Test
+    public void functionHasItsOwnScope() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("var v = \"Failed\"; function func() { var v; if (v==null) return \"Test\"; } return func();");
+        assertEquals("Test", variable.getValue());
+    }
 
-	@Test
-	public void variablesDefinedInFunctionAreNotVisibleOutside() throws ExecutionException, IOException {
-		try {
-			executeScript("function func() { var v = \"Test\"; } func(); if (v != null) return \"Failed\";");
-			fail("Expected IllegalSyntaxException");
-		} catch (IllegalSyntaxException exp) {
-			// Expected
-		}
+    @Test
+    public void variablesDefinedInFunctionAreNotVisibleOutside() throws ExecutionException, IOException {
+        try {
+            executeScript("function func() { var v = \"Test\"; } func(); if (v != null) return \"Failed\";");
+            fail("Expected IllegalSyntaxException");
+        } catch (IllegalSyntaxException exp) {
+            // Expected
+        }
+    }
 
-	}
+    @Test
+    public void recursiveFunctionCall() throws IllegalSyntaxException, IOException, ExecutionException {
+        final Variable variable = executeScript("function factorial(n) { if (n == 0) { return 1; } return n * factorial(n - 1); } return factorial(3);");
+        assertEquals(6, ((Number) variable.getValue()).intValue());
+    }
 }
--- test/com/gempukku/minecraft/automation/lang/ProgramTest.java
+++ test/com/gempukku/minecraft/automation/lang/ProgramTest.java
@@ -10,31 +10,31 @@ import java.io.StringReader;
 import static org.junit.Assert.assertEquals;
 
 public class ProgramTest {
-	protected Variable executeScript(ScriptExecutable exec) throws ExecutionException {
-		CallContext context = new CallContext(null, false, true);
-		ExecutionContext executionContext = initExecutionContext();
-		executionContext.stackExecutionGroup(context, exec.createExecution(context));
+    protected Variable executeScript(ScriptExecutable exec) throws ExecutionException {
+        CallContext context = new CallContext(null, false, true);
+        ExecutionContext executionContext = initExecutionContext();
+        executionContext.stackExecutionGroup(context, exec.createExecution(context));
 
-		while (!executionContext.isFinished())
-			executionContext.executeNext();
+        while (!executionContext.isFinished())
+            executionContext.executeNext();
 
-		assertEquals(0, executionContext.getMemoryUsage());
-		assertEquals(0, executionContext.getStackTraceSize());
+        assertEquals(0, executionContext.getMemoryUsage());
+        assertEquals(0, executionContext.getStackTraceSize());
 
-		return executionContext.getReturnValue();
-	}
+        return executionContext.getReturnValue();
+    }
 
-	protected ExecutionContext initExecutionContext() {
-		ExecutionContext executionContext = new MinecraftComputerExecutionContext(new ServerComputerData(0, 0, 0, 0, 0, 0, "owner", "computerType"));
-		executionContext.addPropertyProducer(Variable.Type.MAP, new MapPropertyProducer());
-		executionContext.addPropertyProducer(Variable.Type.OBJECT, new ObjectPropertyProducer());
-		executionContext.addPropertyProducer(Variable.Type.LIST, new ListPropertyProducer());
-		executionContext.addPropertyProducer(Variable.Type.STRING, new StringPropertyProducer());
-		return executionContext;
-	}
+    protected ExecutionContext initExecutionContext() {
+        ExecutionContext executionContext = new MinecraftComputerExecutionContext(new ServerComputerData(0, "owner", "computerType"));
+        executionContext.addPropertyProducer(Variable.Type.MAP, new MapPropertyProducer());
+        executionContext.addPropertyProducer(Variable.Type.OBJECT, new ObjectPropertyProducer());
+        executionContext.addPropertyProducer(Variable.Type.LIST, new ListPropertyProducer());
+        executionContext.addPropertyProducer(Variable.Type.STRING, new StringPropertyProducer());
+        return executionContext;
+    }
 
-	protected Variable executeScript(String script) throws IllegalSyntaxException, IOException, ExecutionException {
-		ScriptExecutable exec = new ScriptParser().parseScript(new StringReader(script));
-		return executeScript(exec);
-	}
+    protected Variable executeScript(String script) throws IllegalSyntaxException, IOException, ExecutionException {
+        ScriptExecutable exec = new ScriptParser().parseScript(new StringReader(script));
+        return executeScript(exec);
+    }
 }
